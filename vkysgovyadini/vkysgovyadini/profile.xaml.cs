477119a3a3fd1ac454b8e3e4361f0a0309f684fb
Kamipuza <91901774+Kamipuza@users.noreply.github.com>
2022-12-18 03:25:25 +0700

    sleep
    i dont no(

---
+++ vkysgovyadini/vkysgovyadini/profile.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vkysgovyadini
+{
+    /// <summary>
+    /// Логика взаимодействия для profile.xaml
+    /// </summary>
+    public partial class profile : Page
+    {
+        public profile()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
