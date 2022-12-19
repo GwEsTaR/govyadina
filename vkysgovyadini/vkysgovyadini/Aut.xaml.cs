feec32d4c92240057ad0b556d8b635f37b1094e5
VanimaDa <97492115+VanimaDa@users.noreply.github.com>
2022-12-19 15:41:17 +0700

    Авторизация + Два окна 2 сессии

---
+++ vkysgovyadini/vkysgovyadini/Aut.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vkysgovyadini
+{
+    /// <summary>
+    /// Логика взаимодействия для Aut.xaml
+    /// </summary>
+    public partial class Aut : Window
+    {
+        bool isLogin =  false;
+        public Aut()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Login(object sender, RoutedEventArgs e)
+        {
+            BaZeModelEntities db = new BaZeModelEntities();
+            string Pochta = MAILR.Text;
+            string PAROL = PasswordR.Password;
+            
+            try 
+            { 
+                var curUser = db.Пользователи.Where((u) =>u.Пароль == PAROL && u.mail == Pochta).Single();
+                var curRolde = db.Роли.Where((u) => u.idРоль == curUser.idРоль).Single();
+
+                if (curRolde.idРоль == 1)
+                {
+                    MessageBox.Show("Успешно!", $"Привет, {curUser.ФИО}!");
+
+
+                    this.Hide();
+                    profile mainWindow = new profile();
+                    mainWindow.Show();
+                }
+                if (curRolde.idРоль == 2)
+                {
+                    MessageBox.Show("Успешно!", $"Привет, {curUser.ФИО}!");
+
+
+                    this.Hide();
+                    oknoorg mainWindow = new oknoorg();
+                    mainWindow.Show();
+                }
+                if (curRolde.idРоль == 3)
+                {
+                    MessageBox.Show("Успешно!", $"Привет, {curUser.ФИО}!");
+
+
+                    this.Hide();
+                    oknoorg mainWindow = new oknoorg();
+                    mainWindow.Show();
+                }
+                if (curRolde.idРоль == 4)
+                {
+                    MessageBox.Show("Успешно!", $"Привет, {curUser.ФИО}!");
+
+
+                    this.Hide();
+                    oknoorg mainWindow = new oknoorg();
+                    mainWindow.Show();
+                }
+
+
+            }
+            catch
+            {
+              MessageBox.Show("Ошибка!", $"Неверный логин или пароль!");
+            }
+            
+        }
+    }
+}
---
+++ vkysgovyadini/vkysgovyadini/juriANDmoderi.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vkysgovyadini
+{
+    /// <summary>
+    /// Логика взаимодействия для juriANDmoderi.xaml
+    /// </summary>
+    public partial class juriANDmoderi : Window
+    {
+        public juriANDmoderi()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
