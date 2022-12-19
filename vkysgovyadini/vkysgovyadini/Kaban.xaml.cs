e2faaf47f1a2feded4caf97eebf3a2f154f1cf36
Kamipuza <91901774+Kamipuza@users.noreply.github.com>
2022-12-19 09:38:50 +0700

    Kaban window

--- vkysgovyadini/vkysgovyadini/Kaban.xaml.cs
+++ vkysgovyadini/vkysgovyadini/Kaban.xaml.cs
@@ -24,5 +24,10 @@ namespace vkysgovyadini
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
